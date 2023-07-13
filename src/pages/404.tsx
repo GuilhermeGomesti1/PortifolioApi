0b1cceab1f504d3dd4dd44374bdf54a58f11d62f
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-07-13 16:19:05 -0300

    pag404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,24 +1,33 @@
-import { Title } from "@material-ui/icons";
+
 import Head from "next/head";
 import Link from "next/link";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: "500",
+});
 
 const NotFound = () => {
-    return (
-        <>
-        <Head><title> 404 </title></Head>
-        <div>
-           <div>
-            <h1>404</h1>
-  
-        <p>Ops, não conseguimos encontrar essa página!</p>
-        <span>Clique no botão abaixo para ser redirecionado à Página Inicial</span>
+  return (
+    <>
+      <Head>
+        <title> 404 </title>
+      </Head>
+      <div className="flex flex-col items-center text-center mt-12 md:mt-24 gap-8 md:gap-4  px-6 md:px-32">
+        <h1 className="text-5xl smtext-7xl font-bold">404</h1>
 
-            </div>
-            <Link href="/">Ir para página inicial</Link>
-        </div>
-        </>
-        
-    )
-}
+        <p className="flex flex-col gap-8 md:gap-4 ">
+          <span>Ops, não conseguimos encontrar essa página!</span>
+          <span>
+            Clique no botão abaixo para ser redirecionado à Página Inicial
+          </span>
+        </p>
+
+        <Link href="/" className={`${roboto.className} p-4 bg-h-blue-500 rounded-xl text-black mt-5 md:mt-12 w-fit` }>Ir para página inicial</Link>
+      </div>
+    </>
+  );
+};
 
 export default NotFound;
--- src/pages/contatos.tsx
+++ src/pages/contatos.tsx
@@ -1,56 +1,80 @@
 import Head from "next/head";
 
+const Contatos = () => {
+  return (
+    <>
+      <Head>
+        <title>Contatos</title>
+      </Head>
 
-const Contatos = () =>{
-    return(
+      <div className="mt-12 md:mt-24 space-y-8 md:space-y-16 px-6 md:px-32">
+        <h1 className="text-5xl md:text-7xl font-bold text-center ">
+          Contatos
+        </h1>
+        <ul className="table mx-auto space-y-6 md:space-y-8">
+          <li className="md:text-xl">
+            <span className="font-bold">E-mail</span>
+            <div className="flex gap-1 md:gap3 items-center">
+              <a
+                href="mailto:guilherme.gomes.ti1@gmail.com"
+                className="text-sm md:text-lg text-slate-300 underline truncate"
+              >
+                guilherme.gomes.ti1@gmail.com
+              </a>
+            </div>
+          </li>
 
-        <>
-        <Head>
-            <title>Contatos</title>
-        </Head>
-        
-        <div>
-            <h1>Contatos</h1>
-           <ul>
-            <li>
-                <span>E-mail</span>
-                <div>
-                    <a href="mailto:guilherme.gomes.ti1@gmail.com">guilherme.gomes.ti1@gmail.com</a>
-                </div>
-            </li>
+          <li className="md:text-xl">
+            <span className="font-bold">E-mail</span>
+            <div className="flex gap-1 md:gap3 items-center">
+              <a
+                href="mailto:guilherme.gomes.ti1@gmail.com"
+                className="text-sm md:text-lg text-slate-300 underline truncate"
+              >
+                guilherme.gomes.ti1@gmail.com
+              </a>
+            </div>
+          </li>
 
-            <li>
-                <span>E-mail</span>
-                <div>
-                    <a href="mailto:guilherme.gomes.ti1@gmail.com">guilherme.gomes.ti1@gmail.com</a>
-                </div>
-            </li>
+          <li className="md:text-xl">
+            <span className="font-bold">LInkedin</span>
+            <div className="flex gap-1 md:gap3 items-center">
+              <a
+                href="http://Linkedim"
+                className="text-sm md:text-lg text-slate-300 underline truncate"
+              >
+                guilherme.gomes.ti1@gmail.com
+              </a>
+            </div>
+          </li>
 
-            <li>
-                <span>LInkedin</span>
-                <div>
-                    <a href="http://Linkedim">guilherme.gomes.ti1@gmail.com</a>
-                </div>
-            </li>
+          <li className="md:text-xl">
+            <span className="font-bold">github</span>
+            <div className="flex gap-1 md:gap3 items-center">
+              <a
+                href="github"
+                className="text-sm md:text-lg text-slate-300 underline truncate"
+              >
+                guilherme.gomes.ti1@gmail.com
+              </a>
+            </div>
+          </li>
 
-            <li>
-                <span>github</span>
-                <div>
-                    <a href="github">guilherme.gomes.ti1@gmail.com</a>
-                </div>
-            </li>
-
-            <li>
-                <span>E-mail</span>
-                <div>
-                    <a href="mailto:guilherme.gomes.ti1@gmail.com">guilherme.gomes.ti1@gmail.com</a>
-                </div>
-            </li>
-           </ul>
-        </div>
-        </>
-        
-    )
-}
+          <li className="md:text-xl">
+            <span className="font-bold">E-mail</span>
+            <div className="flex gap-1 md:gap3 items-center">
+              <a
+                href="mailto:guilherme.gomes.ti1@gmail.com"
+                className="text-sm md:text-lg text-slate-300 underline truncate"
+              >
+                guilherme.gomes.ti1@gmail.com
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default Contatos;
